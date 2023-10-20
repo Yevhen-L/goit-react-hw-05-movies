35bc0509d6984cb15502c6206ef4cb608ca19186
Yevhen-L <ujeen2101@gmail.com>
2023-10-20 09:24:11 +0300

    add css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,21 @@
 import { NavLink, Route, Routes } from 'react-router-dom';
 import Home from './home/home';
 import Movies from './movies/movies';
+import css from './app.module.css';
 
 export const App = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/movies">Movies</NavLink>
-        </li>
-      </ul>
+      <header>
+        <ul className={css.navHeader}>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
